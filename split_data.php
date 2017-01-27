15806bd51eec057c03bc3ebe184d524807190e0a
Saul Johnson <saul.a.johnson@gmail.com>
2017-01-27 17:11:18 +0000

    Split and add data

---
+++ split_data.php
@@ -0,0 +1,20 @@
+<?php
+
+// Load database from JSON.
+$raw = file_get_contents(__DIR__ . '/data/db.json');
+$database = json_decode($raw, true);
+
+// Randomize entries.
+shuffle($database);
+
+// Count number of entries, half it.
+$entries = count($database);
+$half = floor($entries / 2);
+
+// Divide into two halves, one for training and one for testing.
+$training = array_slice($database, 0, $half);
+$testing = array_slice($database, $half);
+
+// Write training and testing files.
+file_put_contents(__DIR__ . '/data/db_training.json', json_encode($training));
+file_put_contents(__DIR__ . '/data/db_test.json', json_encode($testing));
