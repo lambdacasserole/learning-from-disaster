70105231af55fad60e87b601a7ff6266a0b1f753
Saul Johnson <saul.a.johnson@gmail.com>
2017-01-30 14:32:57 +0000

    Update training script to use new library

--- training/train_network.php
+++ training/train_network.php
@@ -1,19 +1,19 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../neural.php';
 
+use Brainy\NeuralNetwork;
+use Condense\Database;
 use LearningFromDisaster\Console;
 use LearningFromDisaster\Training;
 
 // Load training database.
-$training_db = new \Condense\Database('training');
+$training_db = new Database('training');
 $training_data = $training_db->select();
 $training_data_size = count($training_data);
 
 // Initialize network, turn verbose output off.
 $network = new NeuralNetwork(5, 6, 1);
-$network->setVerbose(false);
 
 // Add training data.
 $message_length = 0;
@@ -21,7 +21,7 @@ $row_number = 1;
 foreach ($training_data as $row) {
     Console::erase($message_length); // Remove previous message.
     $message_length = Console::countAndPrint("Adding row $row_number of $training_data_size");
-    $network->addTestData(Training::quantify($row), [intval($row['survived'])]);
+    $network->addTrainingData(Training::quantify($row), [intval($row['survived'])]);
     $row_number++;
 }
 echo "\nFinished adding training data.\n";
@@ -32,7 +32,9 @@ $current_training_round = 0;
 $success = false;
 while (!$success && ++$current_training_round < $max_training_rounds) {
     $success = $network->train(500, 0.4);
-	echo "No success in training round $current_training_round...\n";
+	if (!$success) {
+		echo "No success in training round $current_training_round...\n";
+	}
 }
 
 // Check for success.
@@ -45,7 +47,7 @@ if ($success) {
 }
 
 // Load testing data.
-$testing_db = new \Condense\Database('testing');
+$testing_db = new Database('testing');
 $testing_data = $testing_db->select();
 
 // Test network on other half of data.
