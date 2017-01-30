866bd671a0b4c50ca693d4a7c35eb430481e6504
Saul Johnson <saul.a.johnson@gmail.com>
2017-01-30 15:04:25 +0000

    Make move_data.php more verbose

--- training/move_data.php
+++ training/move_data.php
@@ -2,11 +2,14 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use LearningFromDisaster\Console;
 use Condense\Database;
 
 // Names of databases to move.
 $databases = ['training', 'testing'];
 foreach ($databases as $name) {
+
+    echo "Creating database '$name'...\n";
 
     // Initialize database, delete it if it exists.
     $db = new Database($name);
@@ -16,7 +19,13 @@ foreach ($databases as $name) {
     $entries = json_decode(file_get_contents(__DIR__ . '/../data/data_' . $name . '.json'));
 
     // Insert each entry into database.
+    $total = count($entries);
+    $current = 1;
+    $message_length = 0;
     foreach ($entries as $entry) {
+        Console::erase($message_length);
+        $message_length = Console::countAndPrint("Inserting row $current/$total...");
         $db->insert($entry);
+        $current++;
     }
 }
